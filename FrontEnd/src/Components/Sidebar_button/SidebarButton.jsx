4c3658e57c5dc17cbae752f8e284732fbbb6e68e
Adrian Eduardo Treviño Peña <eduardotrevinop@yahoo.com.mx>
2023-01-05 20:40:36 -0600

    Made Sidebar button with Material ui list button

--- FrontEnd/src/Components/Sidebar_button/SidebarButton.jsx
+++ FrontEnd/src/Components/Sidebar_button/SidebarButton.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './SidebarButton.css'
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
 function SidebarButton(props) {
     return (
-        <div className='SidebarButton' onClick={() => props.setContent(props.title)}>{props.title}</div>
+        <div className='SidebarButton'>
+            <ListItemButton onClick={() => props.setContent(props.title)}>
+                <ListItemText>{props.title}</ListItemText>
+            </ListItemButton>
+            <Divider />
+        </div>
     )
 }
 
