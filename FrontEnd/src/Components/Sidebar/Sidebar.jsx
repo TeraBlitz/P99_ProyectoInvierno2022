c9815d9c8e220fd5ea9a99e4885b9c931fedaf0c
Adrian Eduardo Treviño Peña <eduardotrevinop@yahoo.com.mx>
2023-01-05 20:40:20 -0600

    added Material ui Sidebar

--- FrontEnd/src/Components/Sidebar/Sidebar.jsx
+++ FrontEnd/src/Components/Sidebar/Sidebar.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import './Sidebar.css'
-import SidebarButton from '../Sidebar_button/SidebarButton'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import SidebarButton from '../Sidebar_button/SidebarButton';
+
 
 function Sidebar(props) {
+    const themes = {
+        sidebar: {
+            minWidth: { xs: '80%', sm: '50%', md: '15%', lg: '15%', xl: '100%' },
+            height: '100%',
+            display: 'block',
+            bgcolor: 'primary.light',
+        },
+        noSidebar: {
+            maxWidth: '0px',
+            display: 'none'
+        }
+    }
     return (
-        <div className='Sidebar'>
-            <h3 className='Sidebar__h3--title'>Sidebar</h3>
-            <SidebarButton title="hello" setContent={props.setContent} />
-            <SidebarButton title="world" setContent={props.setContent} />
+        <Box sx={props.sidebar ? themes.sidebar : themes.noSidebar}>
+            <List component="nav">
+                <SidebarButton title="hello" setContent={props.setContent} />
+                <SidebarButton title="world" setContent={props.setContent} />
 
-        </div>
+
+            </List>
+        </Box >
     )
 }
 
