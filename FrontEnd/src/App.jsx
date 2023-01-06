2313b0774c081afaa1e33e3516bde1ff5df835e0
Adrian Eduardo Treviño Peña <eduardotrevinop@yahoo.com.mx>
2023-01-05 20:40:48 -0600

    Toggle Sidebar button

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from 'react'
 import Sidebar from './Components/Sidebar/Sidebar'
 import './App.css'
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/box'
 
 function App() {
     const [content, setContent] = useState("Content")
+    const [sidebar, setSidebar] = useState(false)
+
+
     return (
         <main>
-            <Sidebar setContent={setContent} />
+            <Sidebar setContent={setContent} sidebar={sidebar} />
             <div className='Container__div--content'>
+                <Box onClick={() => setSidebar(!sidebar)} sx={{
+
+                    bgcolor: 'primary.light',
+                    width: '25px',
+                    height: '25px',
+                    padding: '2px',
+                    borderRadius: 2
+
+                }}>
+                    <MenuIcon />
+                </Box>
+
                 <h1>{content}</h1>
             </div>
-        </main>
+        </main >
     )
 }
 
