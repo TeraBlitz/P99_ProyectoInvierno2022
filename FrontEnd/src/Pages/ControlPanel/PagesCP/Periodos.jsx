d7795910ffc6f4276f6cfa03907b57ae63785b32
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-01-17 17:03:00 -0600

    Funcionalidad de agregar con datos lista

--- FrontEnd/src/Pages/ControlPanel/PagesCP/Periodos.jsx
+++ FrontEnd/src/Pages/ControlPanel/PagesCP/Periodos.jsx
@@ -5,10 +5,31 @@ import TarjetasPeriodos from "./tarjetaPeriodo";
 import data from './periodos'
 
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 
 export default function Periodos(){
 
+    // config de modal estilo
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '60%',
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+
+      };
+
+    //arreglo de objetos previo
     const [tarjetas, setTarjetas] = useState([
         {
             id: 18279,
@@ -45,39 +66,171 @@ export default function Periodos(){
                 }
     ])
 
-    const agregaTarjeta= (text) =>{
+    // genera objetos nuevos con valores predeterminados a la listay los renderiza
+    const agregaTarjeta= (props) =>{
 
         const nuevaTarjeta = {
             id : self.crypto.randomUUID(),
-            clave: 'text',
-            status: 'Activo',
-            fechaInicio: 'febrero 15',
-            fechaFin: 'junio 22',
-            fechaInicioInscripcion: 'enero 20',
-            fechaFinInscripcion: 'febrero 7',
-            cursosMax: 2,
-            idiomasMax: 1,
+            clave: clave,
+            status: status,
+            fechaInicio: fechaInicio,
+            fechaFin: fechaFin,
+            fechaInicioInscripcion: fechaII,
+            fechaFinInscripcion: fechaFI ,
+            cursosMax: cursosMax,
+            idiomasMax: idiomasMax,
 
         }
+        console.log('agregaTarjeta')
+        console.log(clave)
 
         setTarjetas([...tarjetas, nuevaTarjeta])
     }
 
-    const funcionTarjetas = tarjetas.map(item =>{
+    // Variables para agregar tarjeta
 
-        return(
-            <TarjetasPeriodos
-                key={item.id}
-                item={item}
-            />
+    const [clave, setClave] = useState("");
+    const [status, setStatus] = useState("");
+    const [fechaInicio, setFechaInicio] = useState("");
+    const [fechaFin, setFechaFin] = useState("");
+    const [fechaII, setFechaII] = useState("");
+    const [fechaFI, setFechaFI]= useState("");
+    const [cursosMax, setCursosMax] = useState("");
+    const [idiomasMax, setIdiomasMax] = useState("");
+
+    const mandarDatos = (e) =>{
+        agregaTarjeta(
+            clave,
+            status,
+            fechaInicio,
+            fechaFin,
+            fechaII,
+            fechaFI,
+            cursosMax,
+            idiomasMax,
         )
-    })
+        console.log(clave)
+        setClave("");
+        setStatus("");
+        setFechaInicio("");
+        setFechaFin("");
+        setFechaII("");
+        setFechaFI("");
+        setCursosMax("");
+        setIdiomasMax("");
+
+        handleClose();
+    }
 
 
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+    setOpen(true);
+    };
+    const handleClose = () => {
+    setOpen(false);
+    };
+
+    const debug = 'debug'
     return(
         <div className="container">
             <h1>Periodos</h1>
-            <Button variant="contained" onClick={agregaTarjeta}>Nuevo Periodo</Button>
+
+            <Button variant="contained" onClick={handleOpen}>Nuevo Periodo</Button>
+            <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Card sx={style}>
+                        <Typography variant="h5" component="div">
+                           Ingrese los nuevos datos
+                        </Typography>
+                        <div className="spacer"></div>
+
+                                <div>
+
+
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Clave"
+                                            defaultValue={debug}
+                                            name="clave"
+                                            onChange={(e) => setClave(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Status"
+                                            defaultValue={debug}
+                                            name="status"
+                                            onChange={(e) => setStatus(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Fecha de inicio"
+                                            defaultValue={debug}
+                                            name="fechaInicio"
+                                            onChange={(e) => setFechaInicio(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="fecha de Fin"
+                                            defaultValue={debug}
+                                            name="fechaFin"
+                                            onChange={(e) => setFechaFin(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Fecha de inicio de incripciones"
+                                            defaultValue={debug}
+                                            name="fechaInicioInscripciones"
+                                            onChange={(e) => setFechaII(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Fecha de fin de inscripciones"
+                                            defaultValue={debug}
+                                            name="fechaFinInscripciones"
+                                            onChange={(e) => setFechaFI(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Cursos Maximos por Alumno"
+                                            defaultValue={debug}
+                                            name="cursosMax"
+
+                                            onChange={(e) => setCursosMax(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Idiomas Max"
+                                            defaultValue={debug}
+                                            name="idiomasMax"
+                                            onChange={(e) => setIdiomasMax(e.target.value)}
+                                            autoFocus
+                                        />
+                                        <Button variant="contained" onClick={mandarDatos}>Nuevo Periodo</Button>
+
+
+
+
+
+
+                                    </div>
+
+
+                    </Card>
+                </Modal>
+
             <div className="card-grid">
 
 
