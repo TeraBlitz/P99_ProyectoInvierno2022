100a6a08970f528dd491c05cf5590a429544f3fd
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-01-17 09:54:58 -0600

    nuevo formato de tarjetas, modal de edicion y formato de textFields. Falta funcionalidad

--- FrontEnd/src/Pages/ControlPanel/PagesCP/TarjetaPeriodo.jsx
+++ FrontEnd/src/Pages/ControlPanel/PagesCP/TarjetaPeriodo.jsx
@@ -10,18 +10,28 @@ import Typography from '@mui/material/Typography';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Modal from '@mui/material/Modal';
 
+import TextField from '@mui/material/TextField';
+
+
 // formato de estilo para los modals
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '80%',
+    width: '60%',
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
 
+  };
+
+  //Funcion que modifica los daors
+  const handleChange = (e) => {
+    console.log(e.target);
+    const { name, value } = e.target;
+    setClase({ ...clase, [name]: value });
   };
 
 export default function TarjetasPeriodos(props){
@@ -47,24 +57,7 @@ export default function TarjetasPeriodos(props){
                 <Typography sx={{ mb: 1.5 }} color="text.secondary">
                 {props.item.status}
                 </Typography>
-
-            </CardContent>
-          <CardActions>
-            <Button size="small" onClick={handleOpen}>Mas Infromacion</Button>
-              {/* Empieza el modal parent*/ }
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Card sx={style}>
-                        <Typography variant="h5" component="div">
-                            Informacion Adicional
-                        </Typography>
-                        <div className="spacer"></div>
-
-                        <h5 className="leyendaFaltas">Fecha de inicio: </h5>
+                <h5 className="leyendaFaltas">Fecha de inicio: </h5>
                         <Typography sx={{ mb: 1.5 }} color="text.secondary">
                             {props.item.fechaInicio}
                           </Typography>
@@ -91,6 +84,99 @@ export default function TarjetasPeriodos(props){
                           <Typography sx={{ mb: 1.5 }} color="text.secondary">
                              {props.item.idiomasMax}
                           </Typography>
+
+            </CardContent>
+          <CardActions>
+            <Button size="small" onClick={handleOpen}>Editar</Button>
+              {/* Empieza el modal parent*/ }
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Card sx={style}>
+                        <Typography variant="h5" component="div">
+                           Ingrese los nuevos datos
+                        </Typography>
+                        <div className="spacer"></div>
+
+                                <div>
+
+
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Clave"
+                                            value={props.item.clave}
+                                            name="clave"
+
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Status"
+                                            value={props.item.status}
+                                            name="status"
+
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Fecha de inicio"
+                                            value={props.item.fechaInicio}
+                                            name="fechaInicio"
+
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="fecha de Fin"
+                                            value={props.item.fechaFin}
+                                            name="fechaFin"
+
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Fecha de inicio de incripciones"
+                                            value={props.item.fechaInicioInscripcion}
+                                            name="fechaInicioInscripciones"
+
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Fecha de fin de inscripciones"
+                                            value={props.item.fechaFinInscripcion}
+                                            name="fechaFinInscripciones"
+
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Cursos Maximos por Alumno"
+                                            value={props.item.cursosMax}
+                                            name="cursosMax"
+
+
+                                            autoFocus
+                                        />
+                                        <TextField
+                                            style={{ paddingBottom: "15px", fontFamily: "arial" }}
+                                            label="Idiomas Max"
+                                            value={props.item.idiomasMax}
+                                            name="idiomasMax"
+
+                                            autoFocus
+                                        />
+
+
+
+
+
+
+
+                                    </div>
 
 
                     </Card>
