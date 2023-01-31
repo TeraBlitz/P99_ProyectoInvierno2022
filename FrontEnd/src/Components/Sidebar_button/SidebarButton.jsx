4a3baeb1e39dc6c3b68895682bf1863b270249f3
NicolasHerreraHdez <a01114972@itesm.mx>
2023-01-31 15:35:59 -0500

    Style changes to match canva design

--- FrontEnd/src/Components/Sidebar_button/SidebarButton.jsx
+++ FrontEnd/src/Components/Sidebar_button/SidebarButton.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemIcon from '@mui/material/ListItemIcon';  
-import Home from '@mui/icons-material/Home';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
-import SchoolIcon from '@mui/icons-material/School';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import { Divider } from '@mui/material';
 
 function SidebarButton(props) {
     const handleClick = () => {
@@ -16,25 +9,13 @@ function SidebarButton(props) {
 
     }
 
-    const iconDict = {
-        'Inicio' : <Home />,
-        'Perfil' : <AccountCircleIcon/>,
-        'Registro Clases' : <PlaylistAddIcon/>,
-        'Mis Clases' : <SchoolIcon/>,
-        'Mis Clases (Profesor)' : <SchoolIcon/>,
-        'Panel de control': <DashboardIcon/>
-    }
-
-    return props.order === 7 ?
-        <ListItemButton onClick={() => handleClick()} sx={{position: 'absolute', bottom: '15px'}}>
+    return props.order === 7 || props.order === 8 ?
+        <ListItemButton onClick={() => handleClick()} sx={{position: 'absolute', bottom: props.order === 7 ? '170px' : '100px'}}>
             <ListItemText>{props.title}</ListItemText>
         </ListItemButton>
     :
-        <ListItemButton onClick={() => handleClick()} >
-            <ListItemIcon sx={{color: 'white'}}>
-                {iconDict[props.title]}
-            </ListItemIcon>
-            <ListItemText sx={{color: 'white'}}>{props.title}</ListItemText>
+        <ListItemButton onClick={() => handleClick()} sx={{fontSize: '18px'}}>
+            <ListItemText sx={{color: 'white', '& .MuiTypography-root': { fontSize: '1.4rem'}}}>{props.title}</ListItemText>
         </ListItemButton>
 }
 
