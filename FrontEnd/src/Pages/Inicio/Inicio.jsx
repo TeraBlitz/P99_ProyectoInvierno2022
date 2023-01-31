3b2f172fdceb88afb6537973af55a062f1438473
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-01-31 13:59:40 -0600

    boilerplate del diseño del cliente

--- FrontEnd/src/Pages/Inicio/Inicio.jsx
+++ FrontEnd/src/Pages/Inicio/Inicio.jsx
@@ -8,10 +8,39 @@ export default function Inicio(){
     return(
         <div className="container">
             <div className="container2">
-                <h1 className="tittle">Proyecto 99</h1>
+
                 <img className="logo" src={logo}></img>
+                <h3 className="headerText">"La unión hace la fuerza"</h3>
+            </div>
+            <br></br>
+            <div className="containerBody">
+
+                <div className="textColumn">
+                    <h3>¿Quiénes somos?</h3>
+                    <p>Somos un grupo de jóvenes buscando combatir, prevenir y disminuir la deserción escolar en comunidades urbano marginadas, ofreciendo  actividades extracurriculares que fomenten el talento y el desarrollo integral de las personas.</p>
+                </div>
+
+                <div className="textColumn">
+                    <h3>¿Qué ofrecemos?</h3>
+                    <p>Gratuidad: sin cuotas de inscripción, materiales accesibles.
+                        Clases personalizadas: cursos  adaptables a los alumnos y su ritmo de aprendizaje.
+                        Variedad: contenido según los intereses y necesidades de los alumnos.
+                    </p>
+                </div>
+
+                <div className="textColumn">
+                    <h3>Nuestros cursos</h3>
+                    <p>Reforzamiento académico (asesorías 1 a 1 para reforzar conocimientos académicos)
+                        Talleres (artísticos, culturales y deportivos)
+                        Cursos de Idiomas  (variedad de lenguas extranjeras disponibles)
+                    </p>
+                </div>
+
             </div>
 
+            <div className="footerContainer">
+
+            </div>
         </div>
     )
 }
