6ec5fdf544868374dae93709ddd6e95a7bd3f559
JamirLeal <a00826275@itesm.mx>
2023-02-27 14:36:15 -0600

    Ran eslint on PanelCard and PanelInfo.

--- FrontEnd/src/Components/ControlPanel/PanelInfo.jsx
+++ FrontEnd/src/Components/ControlPanel/PanelInfo.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-const PanelInfo = ({title, data, bgColor,num}) => {
+function PanelInfo({
+  title, bgColor, num,
+}) {
   return (
-    <Card sx={{ minWidth: 275 }} >
-    <CardContent sx={{backgroundColor: bgColor}}>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent sx={{ backgroundColor: bgColor }}>
         <Typography variant="h6" sx={{ color: 'white', fontWeight: 700, mb: 0 }}>
-        {title}
+          {title}
         </Typography>
-        <Typography variant="h4" sx={{ color: 'white'}}>{num}</Typography>
-    </CardContent>
-</Card>
-  )
+        <Typography variant="h4" sx={{ color: 'white' }}>{num}</Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default PanelInfo
+export default PanelInfo;
