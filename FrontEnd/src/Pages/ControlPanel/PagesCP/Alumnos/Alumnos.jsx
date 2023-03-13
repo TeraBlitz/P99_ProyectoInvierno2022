d2d2b2ac533e77325f1836487e9647d54c04d489
JamirLeal <a00826275@itesm.mx>
2023-03-13 16:43:37 -0600

    Separated modal content from Alumnos.

--- FrontEnd/src/Pages/ControlPanel/PagesCP/Alumnos/Alumnos.jsx
+++ FrontEnd/src/Pages/ControlPanel/PagesCP/Alumnos/Alumnos.jsx
@@ -1,14 +1,11 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import {
-  Button, Modal, TextField, Box, Typography,
-} from '@mui/material';
+import React, { useState, useEffect } from 'react';
 import { deleteStudent, getStudents, updateStudent } from '../../../../api/students';
 import { getPeriodos } from '../../../../api/Periodos';
 import { getClassStudent } from '../../../../api/classStudent';
 import { alumnoVacio } from '../../../../utils/constants';
-import MasInformacionAlumno from '../../../../Components/ControlPanel/Alumnos/MasInformacionAlumno';
 import HeaderAlumnos from '../../../../Components/ControlPanel/Alumnos/HeaderAlumnos';
 import BodyAlumnos from '../../../../Components/ControlPanel/Alumnos/BodyAlumnos';
+import ModalAlumno from '../../../../Components/ControlPanel/Alumnos/ModalAlumno';
 
 export default function Alumnos() {
   const [studentData, setStudentData] = useState([]);
@@ -103,134 +100,6 @@ export default function Alumnos() {
     getAlumnos();
   };
 
-  const bodyEditar = (
-    <div
-      style={{
-        position: 'absolute',
-        width: 260,
-        height: 620,
-        backgroundColor: '#fefefd',
-        top: '48%',
-        left: '50%',
-        transform: 'translate(-48%, -50%)',
-        border: '4px solid  rgb(165, 165, 180)',
-        margin: 'auto',
-        borderRadius: '10px',
-        padding: '20px',
-      }}
-    >
-      <h3
-        style={{ paddingBottom: '15px', marginTop: '5px', fontFamily: 'arial' }}
-        align="center"
-      >
-        Actualizar Alumnos
-      </h3>
-      <TextField
-        style={{ paddingBottom: '15px', fontFamily: 'arial' }}
-        label="Nombre"
-        onChange={handleChange}
-        name="nombre"
-        value={alumnoSeleccionado?.nombre}
-        autoFocus
-      />
-      <br />
-      <TextField
-        style={{ paddingBottom: '15px', fontFamily: 'arial' }}
-        label="Apellido paterno"
-        onChange={handleChange}
-        name="apellido_paterno"
-        value={alumnoSeleccionado?.apellido_paterno}
-      />
-      <TextField
-        style={{ paddingBottom: '15px', fontFamily: 'arial' }}
-        label="Apellido materno"
-        onChange={handleChange}
-        name="apellido_materno"
-        value={alumnoSeleccionado?.apellido_materno}
-      />
-      <br />
-      <TextField
-        style={{ paddingBottom: '15px', fontFamily: 'arial' }}
-        label="Estado"
-        onChange={handleChange}
-        name="estado"
-        value={alumnoSeleccionado?.estado}
-      />
-      <TextField
-        style={{ paddingBottom: '15px', fontFamily: 'arial' }}
-        label="Ciudad"
-        onChange={handleChange}
-        name="ciudad"
-        value={alumnoSeleccionado?.ciudad}
-      />
-      <br />
-      <TextField
-        style={{ paddingBottom: '15px', fontFamily: 'arial' }}
-        label="Escolaridad"
-        onChange={handleChange}
-        name="escolaridad"
-        value={alumnoSeleccionado?.escolaridad}
-      />
-      <TextField
-        style={{ paddingBottom: '15px', fontFamily: 'arial' }}
-        label="Ultima escuela"
-        onChange={handleChange}
-        name="ultima_escuela"
-        value={alumnoSeleccionado?.ultima_escuela}
-      />
-      <br />
-      <br />
-      <div align="center">
-        <Button color="primary" onClick={modalSubmit}>
-          Editar
-        </Button>
-        <Button onClick={abrirCerrarModal} color="error">
-          Cancelar
-        </Button>
-      </div>
-    </div>
-  );
-
-  const bodyEliminar = (
-    <div
-      style={{
-        position: 'absolute',
-        width: 260,
-        height: 280,
-        backgroundColor: '#fefefd',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        border: '4px solid  rgb(165, 165, 180)',
-        margin: 'auto',
-        borderRadius: '10px',
-        padding: '20px',
-      }}
-    >
-      <h3
-        style={{ paddingBottom: '15px', marginTop: '5px', fontFamily: 'arial' }}
-        align="center"
-      >
-        Eliminar alumno
-      </h3>
-      <Typography style={{ align: 'justify', fontFamily: 'arial' }}>
-        {`El alumno llamado ${alumnoSeleccionado?.nombre} y
-        todo lo relacionado a el se va a eliminar por completo. No vas a poder
-        acceder a estos datos de nuevo.`}
-      </Typography>
-      <br />
-      <br />
-      <div align="center">
-        <Button color="error" onClick={modalSubmit}>
-          Confirmar
-        </Button>
-        <Button onClick={abrirCerrarModal} color="primary">
-          Cancelar
-        </Button>
-      </div>
-    </div>
-  );
-
   const handleSelectChange = async (event) => {
     let array = [];
     let array2 = [];
@@ -268,15 +137,14 @@ export default function Alumnos() {
         data={studentData}
         seleccionarConsola={seleccionarAlumno}
       />
-      <Modal open={openModal} onClose={abrirCerrarModal}>
-        <MasInformacionAlumno consolaSeleccionada={alumnoSeleccionado} />
-      </Modal>
-      <Modal open={openModal} onClose={abrirCerrarModal}>
-        {bodyEditar}
-      </Modal>
-      <Modal open={openModal} onClose={abrirCerrarModal}>
-        {bodyEliminar}
-      </Modal>
+      <ModalAlumno
+        handleChange={handleChange}
+        openModal={openModal}
+        setOpenModal={abrirCerrarModal}
+        alumnoSeleccionado={alumnoSeleccionado}
+        currentOperation={currentOperation}
+        onSubmit={modalSubmit}
+      />
     </div>
   );
 }
