7e43ca78373ff70435a1fa08dceffa51a69cac62
JamirLeal <a00826275@itesm.mx>
2023-03-30 16:56:47 -0600

    Ran eslint over claseModal

--- FrontEnd/src/Components/Clase/ClaseModal.jsx
+++ FrontEnd/src/Components/Clase/ClaseModal.jsx
@@ -10,6 +10,15 @@ import Paper from '@mui/material/Paper';
 import { nivelDict } from '../../utils/constants';
 
 function ClaseModal({ clase }) {
+  const horario = [
+    { dia: 'Lunes', hora: clase.lunes },
+    { dia: 'Martes', hora: clase.martes },
+    { dia: 'Miércoles', hora: clase.miercoles },
+    { dia: 'Jueves', hora: clase.jueves },
+    { dia: 'Viernes', hora: clase.viernes },
+    { dia: 'Sábado', hora: clase.sabado },
+  ];
+
   return (
     <Box
       sx={{
@@ -24,76 +33,44 @@ function ClaseModal({ clase }) {
       }}
     >
       <Typography variant="h5">
-        {clase.clave}
-        .
-        {' '}
-        {clase.nombre_curso}
+        {`${clase.clave}.${clase.nombre_curso}`}
       </Typography>
       <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>Sobre el curso</Typography>
       <Typography>
         <strong>Periodo:</strong>
-        {' '}
-        {clase.clavePeriodo}
+        {` ${clase.clavePeriodo}`}
       </Typography>
       <Typography>
         <strong>Area:</strong>
-        {' '}
-        {clase.area}
+        {` ${clase.area}`}
       </Typography>
       <Typography>
         <strong>Nivel:</strong>
-        {' '}
-        {nivelDict[clase.nivel]}
+        {` ${nivelDict[clase.nivel]}`}
       </Typography>
       <Typography>
         <strong>Modalidad:</strong>
-        {' '}
-        {clase.modalidad}
+        {` ${clase.modalidad}`}
       </Typography>
       <Typography>
         <strong>Horario:</strong>
-        {' '}
+        {` `}
       </Typography>
       <TableContainer component={Paper} sx={{ my: 1 }}>
         <Table size="small">
           <TableBody>
-            <TableRow sx={{ '&:last-child td': { border: 0 } }}>
-              <TableCell align="center"><strong>Lunes</strong></TableCell>
-              <TableCell align="center">{clase.lunes ? clase.lunes : '-'}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td': { border: 0 } }}>
-              <TableCell align="center">
-                <strong>Martes</strong>
-                {' '}
-              </TableCell>
-              <TableCell align="center">{clase.martes ? clase.martes : '-'}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td': { border: 0 } }}>
-              <TableCell align="center"><strong>Miércoles</strong></TableCell>
-              <TableCell align="center">{clase.miercoles ? clase.miercoles : '-'}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td': { border: 0 } }}>
-              <TableCell align="center"><strong>Jueves</strong></TableCell>
-              <TableCell align="center">{clase.jueves ? clase.jueves : '-'}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td': { border: 0 } }}>
-              <TableCell align="center"><strong>Viernes</strong></TableCell>
-              <TableCell align="center">{clase.viernes ? clase.viernes : '-'}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td': { border: 0 } }}>
-              <TableCell align="center"><strong>Sábado</strong></TableCell>
-              <TableCell align="center">{clase.sabado ? clase.sabado : '-'}</TableCell>
-            </TableRow>
+            {horario.map((h, i) => (
+              <TableRow key={i} sx={{ '&:last-child td': { border: 0 } }}>
+                <TableCell align="center"><strong>{h.dia}</strong></TableCell>
+                <TableCell align="center">{h.hora ? h.hora : '-'}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
       <Typography>
         <strong>Cupos:</strong>
-        {' '}
-        ¡Curso
-        {' '}
-        {(Number(clase.cupo_actual) / Number(clase.cupo_maximo) * 100).toFixed()}
-        % lleno!
+        {` ¡Curso ${(Number(clase.cupo_actual) / Number(clase.cupo_maximo) * 100).toFixed()} % lleno!`}
       </Typography>
     </Box>
   );
