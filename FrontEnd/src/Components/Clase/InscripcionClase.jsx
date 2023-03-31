b4ea56439fc5209f9ebde4b5c3021b5b3e62ea4f
JamirLeal <a00826275@itesm.mx>
2023-03-31 00:07:47 -0600

    Ran eslint

--- FrontEnd/src/Components/Clase/InscripcionClase.jsx
+++ FrontEnd/src/Components/Clase/InscripcionClase.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import {
   classTemplate,
 } from '../../utils/constants';
-import HeaderInscripcionClase from '../../Components/Clase/HeaderInscripcionClase';
-import BodyInscripcionClase from '../../Components/Clase/BodyInscripcionClase';
-import ModalInscripcionClase from '../../Components/Clase/ModalInscripcionClase';
+import HeaderInscripcionClase from './HeaderInscripcionClase';
+import BodyInscripcionClase from './BodyInscripcionClase';
+import ModalInscripcionClase from './ModalInscripcionClase';
 import { getWaitList } from '../../api/waitList';
 import { getStudents } from '../../api/students';
 
 function InscripcionClase({
-  data, abrirCerrarModal, resetClases, dataPeriodo, handleSelectChange, 
+  data, abrirCerrarModal, resetClases, dataPeriodo, handleSelectChange,
   profesorList, seleccionarClase, clase, currentProfesor,
-  handleChange, handleChangeProfesor, currentOperation, openModal
+  handleChange, handleChangeProfesor, currentOperation, openModal,
 }) {
   const [currentClase, setCurrentClase] = useState(null);
   const [currentWaitList, setCurrentWaitList] = useState(null);
@@ -24,11 +24,11 @@ function InscripcionClase({
         obj[student._id] = student;
         return obj;
       }, {});
-  
+
       const responseWaitList = await getWaitList();
       const waitList = await responseWaitList.json();
       const result = [];
-  
+
       waitList.forEach((inWaitList) => {
         if (inWaitList.idClase === clase._id && studentsById[inWaitList.idAlumno]) {
           const student = studentsById[inWaitList.idAlumno];
@@ -39,7 +39,7 @@ function InscripcionClase({
           });
         }
       });
-  
+
       result.sort((a, b) => (a > b ? 1 : a < b ? -1 : 0));
       setCurrentWaitList(result);
       setCurrentClase(clase);
